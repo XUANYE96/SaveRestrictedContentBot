2fab713e9bbe7bb9a2d6bf903e436a1fae20df1b
XUANYE96 <yeexuanr@gmail.com>
2024-07-23 21:56:15 +0800

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -13,9 +13,9 @@ from pyrogram.errors import FloodWait
 
 from ethon.telefunc import force_sub
 
-ft = f"To use this bot you've to join @{fs}."
+ft = f"要使用此机器人，您必须加入 @{fs}。"
 
-message = "Send me the message link you want to start saving from, as a reply to this message."
+message = "请发送您要开始保存的消息链接，作为对此消息的回复。"
 
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
@@ -33,7 +33,7 @@ async def clone(event):
     if s == True:
         await event.reply(r)
         return
-    edit = await event.reply("Processing!")
+    edit = await event.reply("处理中！")
     try:
         if 't.me/+' in link:
             q = await join(userbot, link)
@@ -42,8 +42,7 @@ async def clone(event):
         if 't.me/' in link:
             await get_msg(userbot, Bot, Drone, event.sender_id, edit.id, link, 0)
     except FloodWait as fw:
-        return await Drone.send_message(event.sender_id, f'Try again after {fw.x} seconds due to floodwait from telegram.')
+        return await Drone.send_message(event.sender_id, f'由于 Telegram 的 FloodWait，请在 {fw.x} 秒后再试。')
     except Exception as e:
         print(e)
-        await Drone.send_message(event.sender_id, f"An error occurred during cloning of `{link}`\n\n**Error:** {str(e)}")
-    
+        await Drone.send_message(event.sender_id, f"克隆 `{link}` 时发生错误\n\n**错误:** {str(e)}")
